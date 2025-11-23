d332acf6f98081b6bc51b85f3cbd088ec4a1ff49
aryaaapplg <ibu.masarya@gmail.com>
2025-11-23 05:07:12 +0000

    menambah sistematisasi untuk menu transaksi

--- backend/app/Models/Transaksi.php
+++ backend/app/Models/Transaksi.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
-    //
+    protected $table = "transaksis";
+
+    protected $fillable = 
+    [
+        'tipe',
+        'deskripsi',
+        'jumlah',
+        'tanggal'
+    ];
 }
