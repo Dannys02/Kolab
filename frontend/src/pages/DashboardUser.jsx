4b30ade86c443d426496755140f32d5aa8f3255f
Dannys02 <webdannys@gmail.com>
2025-12-04 12:16:49 +0700

    final : penyempurnaan loading dan navigate register

--- frontend/src/pages/DashboardUser.jsx
+++ frontend/src/pages/DashboardUser.jsx
@@ -15,7 +15,7 @@ const UserDashboard = ({ onLogout }) => {
     useEffect(() => {
       const timer = setTimeout(() => {
         setLoadingPage(false);
-      }, 5000) // Atur loading menjadi 1.5 detik (sesuaikan)
+      }, 4500) // Atur loading menjadi 1.5 detik (sesuaikan)
 
       return () => clearTimeout(timer); // Cleanup function
     }, []) // Dependency array kosong, hanya berjalan saat mount
@@ -371,7 +371,7 @@ const UserDashboard = ({ onLogout }) => {
                     </button>
                 </nav>
 
-                <div className="absolute bottom-10 left-6 right-6">
+                <div className="absolute bottom-20 md:bottom-[150px] left-6 right-6">
                     <div className="bg-white bg-opacity-10 rounded-xl p-3 mb-3 flex items-center gap-3 backdrop-blur-sm">
                         <div className="w-8 h-8 bg-green-400 rounded-full flex items-center justify-center text-white font-bold text-xs shadow-sm">
                             {user?.name?.charAt(0).toUpperCase()}
@@ -402,7 +402,7 @@ const UserDashboard = ({ onLogout }) => {
                                 </p>
                             </div>
                             {hasBiodata && (
-                                <span className="px-4 py-2 bg-green-100 text-green-700 rounded-full text-sm font-bold border border-green-200">
+                                <span className="px-4 py-2 bg-green-100 text-green-700 rounded-full text-xs whitespace-nowrap font-bold border border-green-200">
                                     SISWA AKTIF
                                 </span>
                             )}
